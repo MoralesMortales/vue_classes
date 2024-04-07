99410a0831af92aac88464405cc5b1de99dfa329
CarlosMorales <carlos52.7624@gmail.com>
2024-04-07 02:03:05 -0400

    c2

--- vuejs clase 2 - Copy/clase_2/src/components/card.vue
+++ vuejs clase 2 - Copy/clase_2/src/components/card.vue
@@ -13,14 +13,20 @@ import { defineProps } from 'vue';
 
 <template>
     
-    <div class="">
+    <div class="flex-col border-4 border-black hover:scale-110 hover:transition transition">
+
         <div>
-            <img :src="character.image" alt="Image" class="p-4">
+        
+            <img :src="character.image" alt="Image" class="p-5 bg-zinc-600 h-full">
+        
         </div>
         
-        <div class="p-5 text-3xl">
+        <div class="text-3xl snap-center h-20 bg-slate-500 flex-col flex justify-center">
+
             <h3>{{character.name}}</h3>
+
         </div>
 
     </div>
+    
 </template>
--- vuejs clase 2 - Copy/clase_2/src/views/HomeView.vue
+++ vuejs clase 2 - Copy/clase_2/src/views/HomeView.vue
@@ -37,19 +37,21 @@ loadCharacters();
       <h1 class="text-5xl text-rose-950">Rick and Morty API</h1>
     </div>
 
-    <div class="mb-8 grid grid-cols-2 lg:grid-cols-4 gap-12 mx-auto">
+    <div class="mb-8 grid grid-cols-2 lg:grid-cols-4 gap-12 mx-auto ">
 
-      <div v-for="personaje in character" :key="character.id" class="bg-red-700 flex justify-center text-center rounded-lg  shadow-md">
+      <div v-for="personaje in character" :key="character.id" class="bg-black flex relative hover:scale hover:transition transition justify-center text-center rounded-lg shadow-md ">
         <card :character="personaje"/>
       </div>
     </div>
 
     <div class="flex flex-row justify-around">
-      <button @click="nextpage" class="p-5 rounded-lg bg-black text-white">Go to next page</button>
-      <span> {{ page }} </span>
-      <button @click="backpage" :disabled="page === 1" class="p-5 rounded-lg bg-black text-white">Go to previous page</button>      
-    </div>
-<!-- : para condiciones especiales z para clases de css -->
+
+      <button @click="backpage" :disabled="page === 1" class="p-5 rounded-lg bg-black text-white hover:transition hover:bg-white hover:text-black">Go to previous page</button>      
+      <span class="rounded-full bg-white self-center p-2 px-3"> {{ page }} </span>
+      <button @click="nextpage" :disabled="page === 42" class="p-5 rounded-lg bg-black text-white hover:transition hover:bg-white hover:text-black">Go to next page</button>
+
+  </div>
+<!-- para condiciones especiales z para clases de css -->
   </div>
 
 
